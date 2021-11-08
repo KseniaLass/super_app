f12924361034befdebaa5211329465ae1a0012c9
Boldinov90 <71153593+Boldinov90@users.noreply.github.com>
2021-11-09 02:57:31 +0300

    s90

--- src/views/Registration.vue
+++ src/views/Registration.vue
@@ -4,14 +4,14 @@
             <div class="form-title">
                 Регистрация
             </div>
-            <label for="email" class="input-label">{{user.emailLabel}}</label>
-            <input v-model="user.email" type="email" id="email" class="input" placeholder="Укажите Ваш Email">
-            <label for="name" class="input-label">{{user.nameLabel}}</label>
-            <input v-model="user.name" type="text" id="name" class="input" placeholder="Укажите Ваше имя">
-            <label for="password" class="input-label">{{user.passwordLabel}}</label>
-            <input v-model="user.password" type="password" id="password" class="input" placeholder="Введите Ваш пароль">
-            <label for="passwordСonfirm" class="input-label">{{user.passwordСonfirmLabel}}</label>
-            <input v-model="user.passwordСonfirm" type="password" id="passwordСonfirm" class="input" placeholder="Введите пароль ещё раз">
+            <label for="email" class="input-label" :class="{label_error: errors.email == true}">{{user.emailLabel}}</label>
+            <input v-model="user.email" type="email" id="email" class="input" :class="{input_error: errors.email == true}" placeholder="Укажите Ваш Email">
+            <label for="name" class="input-label" :class="{label_error: errors.name == true}">{{user.nameLabel}}</label>
+            <input v-model="user.name" type="text" id="name" class="input" :class="{input_error: errors.name == true}" placeholder="Укажите Ваше имя">
+            <label for="password" class="input-label" :class="{label_error: errors.password == true}">{{user.passwordLabel}}</label>
+            <input v-model="user.password" type="password" id="password" class="input" :class="{input_error: errors.password == true}" placeholder="Введите Ваш пароль">
+            <label for="passwordСonfirm" class="input-label" :class="{label_error: errors.passwordСonfirm == true}">{{user.passwordСonfirmLabel}}</label>
+            <input v-model="user.passwordСonfirm" type="password" id="passwordСonfirm" class="input" :class="{input_error: errors.passwordСonfirm == true}" placeholder="Введите пароль ещё раз">
             <div class="btns">
                 <button class="btn">ОТПРАВИТЬ</button>
                 <button class="btn" @click.prevent="goToLogin">ОТМЕНА</button>
@@ -69,7 +69,34 @@
                     this.errors.email = true
                     this.user.emailLabel = 'Пользователь с таким именем уже существует'
                 }else{
-                    console.log('Email свободный')
+                    
+                    if(this.user.password === this.user.passwordСonfirm){
+                        const response = await fetch('https://superapp-boldinov-default-rtdb.firebaseio.com/users.json', {
+                            method: 'POST',
+                            headers: {
+                                'Content-Type': 'application/json'
+                            },
+                            body: JSON.stringify({
+                                email: this.user.email,
+                                name: this.user.name,
+                                password: this.user.password
+                            })
+                        })
+                        await response.json()
+                        this.user.email = ''
+                        this.user.name = ''
+                        this.user.password = ''
+                        if(response){
+                            this.registrationDone = true
+                        }
+                    }else{
+                        this.errors.passwordСonfirm = true
+                        this.user.passwordСonfirmLabel = 'Пароли не совпадают'
+                        this.user.passwordСonfirm = ''
+                    }
+
+
+
                 }
 
 
@@ -153,6 +180,12 @@
         font-family: Arial, Helvetica, sans-serif; 
         font-size: 18px;
         box-shadow: 0px 0px 22px 1px rgba(0, 0, 0, 0.67) inset;  
+    }
+    .label_error{
+        color: red;
+    }
+    .input_error{
+        border: 2px solid red;
     }
     .btns{
         display: flex;
