e2406a1a8b26fcd25fa2b5ec3673ac4f259a4d9f
Boldinov90 <71153593+Boldinov90@users.noreply.github.com>
2021-12-10 21:36:12 +0300

    s90

--- src/views/ToDo.vue
+++ src/views/ToDo.vue
@@ -16,11 +16,11 @@
         
         <div class="tasks-list">
             <div class="task-item">
-                <div class="task-item-id">1</div>
-                <div class="task-item-title">Помыть посуду</div>
-                <div class="task-item-date">07.12.2021</div>
-                <button class="task-item-edit">&#9998; Редактировать</button>
-                <button class="task-item-delete">&otimes; Удалить</button>
+                <div class="task-item-id">{{taskId}}</div>
+                <div class="task-item-title">{{taskName}}</div>
+                <div class="task-item-date">{{createDate}}</div>
+                <button class="task-item-edit">&#9998;</button>
+                <button class="task-item-delete">&otimes;</button>
             </div>
         </div>
     </div>
@@ -33,16 +33,30 @@
                 tasksNav: [
                     {
                         name: 'new',
-                        value: 'Новые'
+                        value: 'Новые задачи'
                     },
                     {
                         name: 'inJob',
-                        value: 'В работе'
+                        value: 'Задачи в работе'
                     },
                     {
                         name: 'done',
                         value: 'Завершенные'
                     }
+                ],
+                tasks: [
+                    {
+                        taskId: 1,
+                        taskName: 'Помыть посуду',
+                        taskDesсription: 'Прекрасно. Опросите водителей. Может, они что видели. Затем свяжитесь с таксомоторной компанией, перешлите им описание беглецов. А я позвоню в Интерпол.',
+                        createDate: '07.12.2021'
+                    },
+                    {
+                        taskId: 2,
+                        taskName: 'Помыть посуду',
+                        taskDesсription: 'Прекрасно. Опросите водителей. Может, они что видели. Затем свяжитесь с таксомоторной компанией, перешлите им описание беглецов. А я позвоню в Интерпол.',
+                        createDate: '07.12.2021'
+                    },
                 ]
             }
         },
@@ -61,7 +75,6 @@
     }
     .tasks-wrapper{
         width: 100%;
-        height: 1000px;
         display: grid;
         grid-template-columns: 2fr 6fr;
     }
@@ -72,9 +85,9 @@
         position: fixed;
         top: 130px;
     }
-    .tasks-nav{
-        margin-left: 20px;
-    }
+    /* .tasks-nav{
+
+    } */
     .tasks-nav-item{
         color: blanchedalmond;
         list-style-type: none;
@@ -88,21 +101,66 @@
         color: #d6a812;
         transition: 0.2s;
     }
-    .tasks-nav-item-active{
+    /* .tasks-nav-item-active{
 
-    }
+    } */
     .tasks-list{
         margin-top: 130px;
         color: blanchedalmond;
+        display: flex;
+        justify-content: center;
     }
     .task-item{
-        background-color: rgb(238, 16, 16);
-        width: 100%;
-        height: 80px;
+        padding-left: 20px;
+        padding-right: 20px;
+        padding-top: 10px;
+        padding-bottom: 10px;
+        background-color: #1E1F1C;
+        box-shadow: 0px 0px 14px 1px rgba(0, 0, 0, 0.603);
+        border-radius: 10px;
+        width: 80%;
         display: grid;
-        grid-template-columns: 1fr 5fr 2fr 2fr 2fr;
+        grid-template-columns: 1fr 6fr 2fr 1fr 1fr;
         justify-content: center;
         align-items: center;
+    }
+    .task-item-id, 
+    .task-item-title,
+    .task-item-date{
+        font-size: 20px;
+        letter-spacing: 2px;
+
+    }
+    .task-item-edit,
+    .task-item-delete{
+        border-radius: 50%;
+        border: none;
+        width: 30px;
+        height: 30px;
+        justify-self: end;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        background-color: #1E1F1C;
+        cursor: pointer;
+        transition: 0.1s;
+    }
+    .task-item-edit{
+        font-size: 30px;
+        color: rgb(59, 235, 103);
+    }
+    .task-item-delete{
+        font-size: 38px;
+        color: red;
+    }
+    .task-item-edit:hover,
+    .task-item-delete:hover{
+        transform: scale(1.2);
+        transition: 0.3s;
+    }
+    .task-item-edit:active,
+    .task-item-delete:active{
+        transform: scale(1);
     }
 
 </style>
